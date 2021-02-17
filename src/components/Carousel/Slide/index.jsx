3a7a94f060d24cad2f88a1411638b2651fcd7186
Evgeniya Kirtoka <evgeniyakirtoka@gmail.com>
2021-02-17 20:42:03 +0200

    Add btns handlers

--- src/components/Carousel/Slide/index.jsx
+++ src/components/Carousel/Slide/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import styles from './slide.module.css';
 
 const Slide = (props) => {
-  const{image, title, description} = props;
+  const { image, title, description } = props;
   return (
-    <div>
-      <img src={image} alt={title}/>
+    <>
+      <div className={styles.imgWrapper}>
+        <div className={styles.responsiveHelper}>
+          <img className={styles.image} src={image} alt={title} />
+        </div>
+      </div>
+
       <h1>{title}</h1>
       <p>{description}</p>
-    </div>
+    </>
   );
-}
+};
 
 export default Slide;
