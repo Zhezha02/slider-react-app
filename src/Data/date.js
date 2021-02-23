64391383c9a89ce552aa0a0798657a05fc88d6ba
Evgeniya <evgeniyakirtoka@gmail.com>
2021-02-23 14:04:52 +0200

    Add database for slider

---
+++ src/Data/date.js
@@ -0,0 +1,46 @@
+export const slidesDB = [
+  {
+    // image: 'https:/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-lagoon-nebula.jpg?t=tn700',
+
+    image:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-lagoon-nebula.jpg?t=tn700',
+    title: 'Lagoon Nebula',
+    description:
+      'The Lagoon Nebula, a vast stellar nursery located 4,000 light-years away, provides an extraordinary tapestry of star birth.',
+  },
+  {
+    image:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-hh24.jpg',
+    title: 'HH24 light saber',
+    description:
+      'The newborn star in Herbig-Haro 24 (HH 24) is hidden from direct view, but jets blasting out along its rotation axis create a series of glowing shock fronts, revealing the new star’s presence.',
+  },
+  {
+    image:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-eagle-nebula.jpg',
+    title: 'Eagle Nebula',
+    description:
+      'Stars are being born deep inside the so-called Pillars of Creation, which are made of cold hydrogen gas laced with dust.',
+  },
+  {
+    image:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-saturn.jpg',
+    title: 'Saturn',
+    description:
+      'Hubble captured exquisite views of Saturn’s ring system and atmospheric details that once could only be provided by spacecraft visiting the distant world.',
+  },
+  {
+    image:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-milky-way-bulge.jpg',
+    title: 'Milky Way Bulge',
+    description:
+      'The Milky Way’s bulge is a crowded and dynamic region of variously aged stars zipping around at different speeds at the heart of our galaxy.',
+  },
+  {
+    image:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-whirlpool-galaxy.jpg',
+    title: 'Whirlpool Galaxy',
+    description:
+      'This image of the Whirlpool Galaxy illustrates a spiral galaxy’s grand design, from its curving spiral arms to its yellowish central core.',
+  },
+];
