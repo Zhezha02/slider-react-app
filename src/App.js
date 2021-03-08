11f6422a201e75c531c42c2c0f8bdad15349f489
Evgeniya <evgeniyakirtoka@gmail.com>
2021-03-08 09:08:47 +0200

    Delete unused files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-// import './App.css';
-// import Carousel from './components/Carousel';
-// import { slidesDB } from './Data/date';
-// import WindowSizes from './components/WindowSizes';
+import './App.css';
+import Carousel from './components/Carousel';
+import { slidesDB } from './Data/date';
 
-// function App () {
-//   return (
-//     <>
-//       {/* <Carousel imageDB={slidesDB} />  */}
-//     </>
-//   );
-// }
-
-// export default App;
-
-import React, { Component } from 'react';
-import UserContext from './contexts/UserContext';
-import PageHeader from './components/PageHeader';
-import Sidebar from './components/Sidebar';
-
-class App extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      user: {
-        id: 1,
-        firstName: 'John',
-        lastName: 'Doe',
-        imageSrc: 'https://www.011global.com/Account/Slices/user-anonymous.png',
-      },
-    };
-  }
-
-  render () {
-    return (
-      <UserContext.Provider value={this.state.user}>
-        <PageHeader />
-        <Sidebar/>
-      </UserContext.Provider>
-    );
-  }
+function App () {
+  return (
+    <>
+      <Carousel imageDB={slidesDB} /> 
+    </>
+  );
 }
 
 export default App;
+
